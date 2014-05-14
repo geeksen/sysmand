7acdff8a343eb7f172f565af6298b2318918a228
Terry Geeksen <geeksen@gmail.com>
2014-05-14 15:23:20 +0000

    configuration

--- lib/example.pl
+++ lib/example.pl
@@ -1,8 +1,17 @@
 #!/usr/bin/perl
 
 use strict;
+use warnings;
+
+use Data::Dumper;
 
 use lib qw(.); use sysmand;
 use vars qw(%cfg); *cfg = \%sysmand::cfg;
 
-print $cfg{path};
+if (0 == &sysmand::load_cfg)
+{
+	print "load_cfg failed\n";
+	exit;
+}
+
+print Dumper(%cfg);
--- lib/sysmand.pm
+++ lib/sysmand.pm
@@ -3,10 +3,69 @@
 package sysmand;
 
 use strict;
+use warnings;
 use vars qw(%cfg);
 
-%cfg = (
-	'path' => '/home/ubuntu/github/sysmand',
-);
+sub load_cfg
+{
+	my @cfg_files = &read_dir_and_find('../cfg/', '\.cfg$');
+	if (0 == scalar @cfg_files)
+	{
+		return 0;
+	}
+
+	foreach my $cfg_file (@cfg_files)
+	{
+		open my $CFG, '<', '../cfg/' . $cfg_file or return 0;
+
+		my $file = '';
+		while (0 != (my $n = read $CFG, my $tmp, 1024)) { $file .= $tmp; }
+		close $CFG;
+
+		$file =~ s/\\r//g;
+		my @lines = split /\n/, $file;
+
+		$cfg_file =~ s/\.cfg$//g;
+		foreach my $line (@lines)
+		{
+			my @pair = split /\=/, $line;
+			$cfg{$cfg_file . '::' . &trim($pair[0])} = &trim($pair[1]);
+		}
+	}
+
+	return 1;
+}
+
+sub read_dir_and_find
+{
+        my $path = $_[0];
+        my $keyword = $_[1];
+
+	if (! -d $path)
+	{
+		return ();
+	}
+
+        opendir(my $D, $path) or die $!;
+        my @files = readdir $D;
+        closedir $D;
+
+        my @found = ();
+        foreach my $file (@files)
+        {
+                if ($file =~ /^\./) { next; }
+                if ($file =~ /$keyword/) { push @found, $file; }
+        }
+
+        return sort @found;
+}
+
+sub trim
+{
+	my $str = $_[0];
+
+	$str =~ s/^\s+|\s+$//g;
+	return $str;
+}
 
 1;
